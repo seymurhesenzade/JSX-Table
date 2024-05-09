ec435cf7ad11eea3184fe4b60967d8f39cf9c276
seymurhesenzade <hasanzadeseymur21@gmail.com>
2024-05-10 01:53:32 +0400

    Page

---
+++ Page/FirstPage/src/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import'./css/Header.css'
+
+const Header = () => {
+  return (
+    <>
+      
+<div  className='header'>
+<div className='title'>Hustlers University</div>
+</div>
+
+    </>
+  )
+}
+
+export default Header
